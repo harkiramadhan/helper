b9615cee56d55702e2b91395920ce8f044465f9b
harkiramadhan <harkiramadhan@gmail.com>
2020-01-03 01:41:57 +0700

    quran helper

---
+++ quran_helper.php
@@ -0,0 +1,348 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function surat($surat){
+    switch($surat){
+        case 1:
+            return "Al Fatihah - الفاتحة";
+            break;
+        case 2:
+            return "Al Baqarah - البقرة";
+            break;
+        case 3:
+            return "Ali Imran - آل عمران";
+            break;
+        case 4:
+            return "An Nisaa - النساء";
+            break;
+        case 5:
+            return "Al Maidah - المائدة";
+            break;
+        case 6:
+            return "Al An’am - الأنعام";
+            break;
+        case 7:
+            return "Al A’raf - الأعراف";
+            break;
+        case 8:
+            return "Al Anfaal - الأنفال";
+            break;
+        case 9:
+            return "At Taubah - التوبة";
+            break;
+        case 10:
+            return "Yunus - يونس";
+            break;
+        case 11:
+            return "Huud - هود";
+            break;
+        case 12:
+            return "Yusuf - يوسف";
+            break;
+        case 13:
+            return "Ar Ra’du - الرعد";
+            break;
+        case 14:
+            return "Ibrahim - ابراهيم";
+            break;
+        case 15:
+            return "Al Hijr - الحجر";
+            break;
+        case 16:
+            return "An Nahl - النحل";
+            break;
+        case 17:
+            return "Al Israa’   =  الإسراء";
+            break;
+        case 18:
+            return "Al Kahfi - الكهف";
+            break;
+        case 19:
+            return "Maryam - مريم";
+            break;
+        case 20:
+            return "Thaahaa - طه";
+            break;
+        case 21:
+            return "Al Anbiyaa - الأنبياء";
+            break;
+        case 22:
+            return "Al Hajj - الحج";
+            break;
+        case 23:
+            return "Al Mu’minun - المؤمنون";
+            break;
+        case 24:
+            return "An Nuur - النور";
+            break;
+        case 25:
+            return "Al Furqaan - الفرقان";
+            break;
+        case 26:
+            return "Asy Syu’ara - الشعراء";
+            break;
+        case 27:
+            return "An Naml - النمل";
+            break;
+        case 28:
+            return "Al Qashash - القصص";
+            break;
+        case 29:
+            return "Al ‘Ankabut -  العنكبوت";
+            break;
+        case 30:
+            return "Ar Ruum - الروم";
+            break;
+        case 31:
+            return "Luqman - لقمان";
+            break;
+        case 32:
+            return "As Sajdah - السجدة";
+            break;
+        case 33:
+            return "Al Ahzab - الأحزاب";
+            break;
+        case 34:
+            return "Saba’ - سبإ";
+            break;
+        case 35:
+            return "Faathir - فاطر";
+            break;
+        case 36:
+            return "Yaa Siin - يس";
+            break;
+        case 37:
+            return "Ash Shaaffat - الصافات";
+            break;
+        case 38:
+            return "Shaad - ص";
+            break;
+        case 39:
+            return "Az Zumar - الزمر";
+            break;
+        case 40:
+            return "Al Ghaafir - غافر";
+            break;
+        case 41:
+            return "Al Fushilat - فصلت";
+            break;
+        case 42:
+            return "Asy Syuura - الشورى";
+            break;
+        case 43:
+            return "Az Zukhruf - الزخرف";
+            break;
+        case 44:
+            return "Ad Dukhaan - الدخان";
+            break;
+        case 45:
+            return "Al Jaatsiyah - الجاثية";
+            break;
+        case 46:
+            return "Al Ahqaaf - الأحقاف";
+            break;
+        case 47:
+            return "Muhammad - محمد";
+            break;
+        case 48:
+            return "Al Fath - الفتح";
+            break;
+        case 49:
+            return "Al Hujuraat - الحجرات";
+            break;
+        case 50:
+            return "Qaaf - ق";
+            break;
+        case 51:
+            return "Adz Dzaariyaat - الذاريات";
+            break;
+        case 52:
+            return "Ath Thuur - الطور";
+            break;
+        case 53:
+            return "An Najm - النجم";
+            break;
+        case 54:
+            return "Al Qamar  = القمر";
+            break;
+        case 55:
+            return "Ar Rahmaan - الرحمن";
+            break;
+        case 56:
+            return "Al Waaqi’ah - الواقعة";
+            break;
+        case 57:
+            return "Al Hadiid - الحديد";
+            break;
+        case 58:
+            return "Al Mujaadalah - المجادلة";
+            break;
+        case 59:
+            return "Al Hasyr - الحشر";
+            break;
+        case 60:
+            return "Al Mumtahanah - الممتحنة";
+            break;
+        case 61:
+            return "Ash Shaff - الصف";
+            break;
+        case 62:
+            return "Al Jumuah - الجمعة";
+            break;
+        case 63:
+            return "Al Munafiqun - المنافقون";
+            break;
+        case 64:
+            return "Ath Taghabun - التغابن";
+            break;
+        case 65:
+            return "Ath Thalaaq - الطلاق";
+            break;
+        case 66:
+            return "At Tahrim - التحريم";
+            break;
+        case 67:
+            return "Al Mulk - الملك";
+            break;
+        case 68:
+            return "Al Qalam - القلم";
+            break;
+        case 69:
+            return "Al Haaqqah - الحاقة";
+            break;
+        case 70:
+            return "Al Ma’aarij - المعارج";
+            break;
+        case 71:
+            return "Nuh - نوح";
+            break;
+        case 72:
+            return "Al Jin - الجن";
+            break;
+        case 73:
+            return "Al Muzammil - المزمل";
+            break;
+        case 74:
+            return "Al Muddastir - المدثر";
+            break;
+        case 75:
+            return "Al Qiyaamah - القيامة";
+            break;
+        case 76:
+            return "Al Insaan - الانسان";
+            break;
+        case 77:
+            return "Al Mursalaat - المرسلات";
+            break;
+        case 78:
+            return "An Naba’ - النبإ";
+            break;
+        case 79:
+            return "An Naazi’at - النازِعات";
+            break;
+        case 80:
+            return "‘Abasa - عبس";
+            break;
+        case 81:
+            return "At Takwir - التكوير";
+            break;
+        case 82:
+            return "Al Infithar  =  الإنفطار";
+            break;
+        case 83:
+            return "Al Muthaffifin - المطففين";
+            break;
+        case 84:
+            return "Al Insyiqaq - الإنشقاق";
+            break;
+        case 85:
+            return "Al Buruuj - البروج";
+            break;
+        case 86:
+            return "Ath Thariq - الطارق";
+            break;
+        case 87:
+            return "Al A’laa - الأعلى";
+            break;
+        case 88:
+            return "Al Ghaasyiah - الغاشية";
+            break;
+        case 89:
+            return "Al Fajr  = الفجر";
+            break;
+        case 90:
+            return "Al Balad - البلد";
+            break;
+        case 91:
+            return "Asy Syams - الشمس";
+            break;
+        case 92:
+            return "Al Lail - الليل";
+            break;
+        case 93:
+            return "Adh Dhuhaa =  الضحى";
+            break;
+        case 94:
+            return "Asy Syarh - الشرح";
+            break;
+        case 95:
+            return "At Tiin - التين";
+            break;
+        case 96:
+            return "Al ‘Alaq - العلق";
+            break;
+        case 97:
+            return "Al Qadr - القدر";
+            break;
+        case 98:
+            return "Al Bayyinah - البينة";
+            break;
+        case 99:
+            return "Az Zalzalah - الزلزلة";
+            break;
+        case 100:
+            return "Al ‘Aadiyah - العاديات";
+            break;
+        case 101:
+            return "Al Qaari’ah - القارعة";
+            break;
+        case 102:
+            return "At Takaatsur  = التكاثر";
+            break;
+        case 103:
+            return "Al ‘Ashr  = العصر";
+            break;
+        case 104:
+            return "Al Humazah - الهمزة";
+            break;
+        case 105:
+            return "Al Fiil - الفيل";
+            break;
+        case 106:
+            return "Quraisy - قريش";
+            break;
+        case 107:
+            return "Al Maa’uun - الماعون";
+            break;
+        case 108:
+            return "Al Kautsar - الكوثر";
+            break;
+        case 109:
+            return "Al Kafirun - الكافرون";
+            break;
+        case 110:
+            return "An Nashr - النصر";
+            break;
+        case 111:
+            return "Al Lahab - اللهب";
+            break;
+        case 112:
+            return "Al Ikhlash  =  الإخلاص";
+            break;
+        case 113:
+            return "Al Falaq - الفلق";
+            break;
+        case 114:
+            return "An Naas - الناس";
+            break;
+    }
+}
